8a955e51748006ee205c99238c2c0ff6ba08d00f
Edwin Paul <saidarajab101@gmail.com>
2023-04-26 21:03:54 +0300

    CRUD lab

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -17,9 +17,28 @@ function QuestionForm(props) {
     });
   }
 
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const questionData = {
+    prompt: formData.prompt,
+    answer1: formData.answer1,
+    answer2: formData.answer2,
+    answer3: formData.answer3,
+    answer4: formData.answer4,
+    correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions",{
+      method: "POST",
+      headers:{
+       "Content-Type": "application/json", 
+      },
+
+    body: JSON.stringify(questionData),
+    })
+    .then((r) => r.json())
+    .then((newQuestion) => props.onAddQuestion(newQuestion));
+    // console.log(formData)
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  const [deletedQuestionId, setDeletedQuestionId] = useState(null);
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, [deletedQuestionId]);
+
+  const handleQuestionDelete =(id) => {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE",
+    })
+    .then(() => {
+      setDeletedQuestionId(id);
+    })
+  }
+  const questionItems = questions.map((question) => (
+    <QuestionItem 
+    key={question.id} 
+    question={question} 
+    onDelete={() => handleQuestionDelete(question.id)}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
